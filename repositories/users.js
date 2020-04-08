2451453af13a483400a371d46d8bd456c2d3db1e
Jean <jean.soro007@gmail.com>
2020-04-08 14:33:14 -0400

    Implement functionality to open Repo data file using nodejs

--- repositories/users.js
+++ repositories/users.js
@@ -1,27 +1,40 @@
 const fs = require('fs');
 
 class UsersRepository {
+
   constructor(filename) {
     if (!filename) {
       throw new Error('Filename required to create repository');
     }
 
     this.filename = filename;
+
     try {
       //check if file exists
       fs.accessSync(this.filename);
     } catch (err) {
+      //if not, create a file
       fs.writeFileSync(this.filename, '[]');
     }
 
+  }
+
+  async getAll() {
+    //Open the file called this.filename Read its contents Parse the contents Return the parsed data
+    return JSON.parse(await fs.promises.readFile(this.filename, { encoding: 'utf8' }));
+
 
   }
 
-  async checkForFile() {
-
-  }
 
 
 }
 
-const repo = new UsersRepository('users.json');
+const test = async () => {
+
+  const repo = new UsersRepository('users.json');
+  await repo.getAll();
+}
+
+test()
+
