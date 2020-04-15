5a8b430cb3c482652483f66e03fd8fc3096408b6
Jean <jean.soro007@gmail.com>
2020-04-15 17:01:32 -0400

    signup validation logic

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const usersRepo = require('./repositories/users');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,15 +18,27 @@ app.get('/', (req, res) => {
   `)
 });
 
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
 
-  req.on('data', data => {
+  const { email, password, passwordConfirmation } = req.body;
 
-  })
+  const existingUser = await usersRepo.getOneBy({ email });
+
+  if (existingUser) {
+    return res.send('Email already in use');
+  }
+
+  if (password !== passwordConfirmation) {
+    return res.send('Password must match');
+  }
+
 
 
   res.send('Account Created')
 })
+
+
+
 
 app.listen(3000, () => {
   console.log('Listening')
